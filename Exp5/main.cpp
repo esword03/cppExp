24da23197dedc77531e7a9c07b2e4a8fb41b9016
esword03 <48149198+esword03@users.noreply.github.com>
2019-11-05 00:00:01 +0800

    Update main.cpp

--- Exp5/main.cpp
+++ Exp5/main.cpp
@@ -3,43 +3,46 @@
 using namespace std;
 void input(Point*);
 int main() {
-	Point* p;
-	input(p);
+    Point* p;
+    input(p);
 }
-Point::Point(int a=0, int b=0) {
-	x = a;
-	y = b;
+Point::Point(int a, int b) {
+    x = a;
+    y = b;
 }
 Circle::Circle(int a,int b,int c):Point(a,b) {
-	radius = c;
+    radius = c;
 }
 Cylinder::Cylinder(int a, int b, int c, int d) : Circle(a, b, c) {
-	height = d;
+    height = d;
 }
 void Point::show() {
-	cout << "(" << getX() << "," << getY() <<")"<< endl;
+    cout << "(" << getX() << "," << getY() <<")"<< endl;
 }
 void Circle::show() {
-	cout << "Բ�����꣺"; Point::show();
-	cout << "Բ�뾶��" << getradius()<<endl;
+    cout << "圆心坐标："; Point::show();
+    cout << "圆半径：" << getradius()<<endl
+         <<"圆面积："<<getarea()<<endl;
 }
 void Cylinder::show() {
-	Circle::show();
-	cout << "Բ����߶ȣ�" << getH()<<endl;
+    Circle::show();
+    cout << "圆柱体高度：" << getH()<<endl
+         <<"圆体积："<<getV()<<endl;
 }
 void input(Point* p) {
-	int value;
-	vector<int> a;
-	cin >> value;
-	a.push_back(value);
-	while (cin.get() != '\n') {
-		cin >> value;
-		a.push_back(value);
-	}
-	if (a.size() == 2)
-		p = new Point(a[0], a[1]);
-	else if (a.size() == 3)
-		p = new Circle(a[0], a[1], a[2]);
-	else if (a.size() == 4)
-		p = new Cylinder(a[0], a[1], a[2], a[3]);
+    int value;
+    vector<int> a;
+    cin >> value;
+    a.push_back(value);
+    while (cin.get() != '\n') {
+        cin >> value;
+        a.push_back(value);
+    }
+    if (a.size() == 2)
+        p = new Point(a[0], a[1]);
+    else if (a.size() == 3)
+        p = new Circle(a[0], a[1], a[2]);
+    else if (a.size() == 4)
+        p = new Cylinder(a[0], a[1], a[2], a[3]);
+    p->show();
 }
