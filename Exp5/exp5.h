9354f84b193eb75424c147ab68f6431acfd624eb
esword03 <48149198+esword03@users.noreply.github.com>
2019-11-05 00:02:49 +0800

    Update exp5.h

--- Exp5/exp5.h
+++ Exp5/exp5.h
@@ -1,30 +1,31 @@
 #include<iostream>
 class Point {
 public:
-	Point(int,int);
-	void show();
-	int getX() { return x; }
-	int getY() { return y; }
+    Point(int,int);
+
+    virtual void show();
+    int getX() { return x; }
+    int getY() { return y; }
 private:
-	int x;
-	int y;
+    int x;
+    int y;
 };
 class Circle :public Point {
 public:
-	Circle(int,int,int);
-	void show();
-	int getarea() {return getX()*getY(); }
-	int getradius() { return radius; }
+    Circle(int,int,int);
+    virtual void show();
+    int getarea() {return 3.14*(radius*radius); }
+    int getradius() { return radius; }
 private:
-	int radius;
+    int radius;
 };
 
 class Cylinder :public Circle {
 public:
-	Cylinder(int, int, int, int);
-	void show();
-	int getH() { return height; }
-	int getV() { return height*getarea(); }
+    Cylinder(int, int, int, int);
+    virtual void show();
+    int getH() { return height; }
+    int getV() { return height*getarea(); }
 private:
-	int height;
+    int height;
 };
